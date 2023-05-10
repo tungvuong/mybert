f55408b11e2d827b4ad60f0c0720db30582fca11
vuong <vuong@vuongs-iMac.local>
2023-05-10 10:05:39 +0300

    my

--- timepoliisi.py
+++ timepoliisi.py
@@ -1,6 +1,7 @@
 import requests
 import requests
 import re
+
 s = requests.Session()
 suggest = s.get('https://asiointi.poliisi.fi/ajanvaraus-fe')
 print(s.cookies.get_dict()['JSESSIONID'])
@@ -135,14 +136,141 @@ response = requests.post(
     json=json_data,
 )
 
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{"participantMultiplier":3,"siteId":"5501","startTime":"2023-07-31T14:40:00Z","ajanvarausServiceType":{"group":{"groupCode":"01","groupPrio":"1","nameFi":"Passit ja henkilökortit","nameSv":"Pass och identitetskort","nameEn":"Passports and identity cards","nameSe":"Opássat ja persovdnagoarttat"},"typeUICode":"0402","typeSystemCode":"AV0499","nameFi":"Passi","nameSv":"Pass","nameEn":"Passport","nameSe":"Pássa","homeDepartmentOnly":false,"caseType":"PASSI","caseSpecificType":null}}'.encode()
-#response = requests.post(
-#    'https://asiointi.poliisi.fi/ajanvaraus-fe/api/prereservation',
-#    cookies=cookies,
-#    headers=headers,
-#    data=data,
-#)
+reservationId=response.json()['reservationId']
+
+
+cookies = {
+    'pol_init': '1',
+    'JSESSIONID': JSESSIONID,
+    'poliisi_asiointi': '!uSeLrFjeYuPSIx4+jBcx640NL8GBbs7jwp3hqsPlgQDXqAAbiUqQo1z7bhNtPem18oUt5rPOwOS0Z65MhnqQ2WaZIqCkZc29PG7SF1oNOQ==',
+    'Snoobisession_asiointi_poliisi_fi': '15477727',
+    'Snoobi30minute_asiointi_poliisi_fi': '15477727',
+    'SnoobiID': '1505577995-b58e1cb1f1ae064ca2751c672a905d1c',
+    'TS01805349': '01489080b5641ce133d2c927ea091dbecc4868616e57921098c7d8d000b961e418e587088c3457ea9d033621e42086fc7fc3c67f2c',
+}
+
+headers = {
+    'Accept': '*/*',
+    'Accept-Language': 'en-GB,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/json',
+    'Origin': 'https://asiointi.poliisi.fi',
+    'Referer': 'https://asiointi.poliisi.fi/ajanvaraus-fe/reserve',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+    'X-CSRF-TOKEN': CSRF,
+    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+}
+
+json_data = {
+    'principalContact': {
+        'contactInfo': {
+            'email': 'vuongthanhtung@live.com',
+            'emailAgain': 'vuongthanhtung@live.com',
+            'phoneNumber': '+358413144891',
+            'phoneNumberAgain': '+358413144891',
+        },
+        'principal': {
+            'name': 'Minh Hue Duong',
+            'firstName': 'Minh Hue',
+            'lastName': 'Duong',
+            'birthDay': '150586-300K',
+        },
+    },
+    'appointment': {
+        'reservationId': reservationId,
+        'topics': [
+            {
+                'ajanvarausServiceType': {
+                    'group': {
+                        'groupCode': '01',
+                        'groupPrio': '1',
+                        'nameFi': 'Passit ja henkilökortit',
+                        'nameSv': 'Pass och identitetskort',
+                        'nameEn': 'Passports and identity cards',
+                        'nameSe': 'Opássat ja persovdnagoarttat',
+                    },
+                    'typeUICode': '0402',
+                    'typeSystemCode': 'AV0499',
+                    'nameFi': 'Passi',
+                    'nameSv': 'Pass',
+                    'nameEn': 'Passport',
+                    'nameSe': 'Pássa',
+                    'homeDepartmentOnly': False,
+                    'caseType': 'PASSI',
+                    'caseSpecificType': None,
+                },
+                'participant': {
+                    'name': 'Minh Hue Duong',
+                    'birthDay': '150586-300K',
+                },
+                'principal': True,
+            },
+            {
+                'ajanvarausServiceType': {
+                    'group': {
+                        'groupCode': '01',
+                        'groupPrio': '1',
+                        'nameFi': 'Passit ja henkilökortit',
+                        'nameSv': 'Pass och identitetskort',
+                        'nameEn': 'Passports and identity cards',
+                        'nameSe': 'Opássat ja persovdnagoarttat',
+                    },
+                    'typeUICode': '0402',
+                    'typeSystemCode': 'AV0499',
+                    'nameFi': 'Passi',
+                    'nameSv': 'Pass',
+                    'nameEn': 'Passport',
+                    'nameSe': 'Pássa',
+                    'homeDepartmentOnly': False,
+                    'caseType': 'PASSI',
+                    'caseSpecificType': None,
+                },
+                'participant': {
+                    'name': 'Vuong Kha Nhu',
+                    'birthDay': '131116A572U',
+                },
+                'principal': False,
+            },
+            {
+                'ajanvarausServiceType': {
+                    'group': {
+                        'groupCode': '01',
+                        'groupPrio': '1',
+                        'nameFi': 'Passit ja henkilökortit',
+                        'nameSv': 'Pass och identitetskort',
+                        'nameEn': 'Passports and identity cards',
+                        'nameSe': 'Opássat ja persovdnagoarttat',
+                    },
+                    'typeUICode': '0402',
+                    'typeSystemCode': 'AV0499',
+                    'nameFi': 'Passi',
+                    'nameSv': 'Pass',
+                    'nameEn': 'Passport',
+                    'nameSe': 'Pássa',
+                    'homeDepartmentOnly': False,
+                    'caseType': 'PASSI',
+                    'caseSpecificType': None,
+                },
+                'participant': {
+                    'name': 'Vuong Thanh Tung',
+                    'birthDay': '030786-2893',
+                },
+                'principal': False,
+            },
+        ],
+    },
+}
+
+response = requests.post(
+    'https://asiointi.poliisi.fi/ajanvaraus-fe/api/appointment',
+    cookies=cookies,
+    headers=headers,
+    json=json_data,
+)
 
 print(response.json())
